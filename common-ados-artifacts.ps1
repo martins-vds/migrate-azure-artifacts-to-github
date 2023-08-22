a07f9fd98fc7bcd75c54615f804b3023056d5c79
Vinny Martins <martins.vds@outlook.com>
2023-08-22 10:07:24 -0600

    add more log msgs

--- common-ados-artifacts.ps1
+++ common-ados-artifacts.ps1
@@ -38,16 +38,23 @@ function GetAdosPackages {
             $feedApi = "https://feeds.dev.azure.com/$org/$project/_apis/packaging/Feeds/$feed/packages"    
         }
 
-        $packagesApi = "$($feedApi)?api-version=7.0&protocolType=$type&includeAllVersions=true&`$top=100&`$skip={0}"
+        $top = 10
+        $packagesApi = "$($feedApi)?api-version=7.0&protocolType=$type&includeAllVersions=true&`$top=$top&`$skip={0}"
 
         $allPackages = @()
         $skip = 0
 
         do {
+            Write-Verbose "Calling Azure DevOps API '$($packagesApi -f $skip)'..."
             $packages = Get -uri "$($packagesApi -f $skip)" -headers $(BuildAdosHeaders $token)
             $allPackages += $packages.value
-            $skip += 100
+            $skip += $top
+            Write-Verbose "Fetched $($packages.count) packages from Azure DevOps feed '$feed' in organization '$org'."
         }while ($packages.count -gt 0)
+
+        Write-Verbose "Fetched $($allPackages.count) packages from Azure DevOps feed '$feed' in organization '$org'."
+
+        Write-Verbose "Filtering packages with internal provenance source..."
 
         $internalPackages = @($allPackages | Where-Object { 
                 $package = $_
@@ -59,6 +66,8 @@ function GetAdosPackages {
 
                 return $package.versions.count -gt 0
             })
+
+        Write-Verbose "Filtered $($internalPackages.count) packages with internal provenance source."
 
         return $internalPackages
     }
